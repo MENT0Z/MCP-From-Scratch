47c8da67330cef12a952b7f054db3cc1f0840893
Madan <mrmrupadhyau@gmail.com>
2026-07-20 11:41:44 +0545

    Implement router and return the respose

---
+++ MCP server/router.py
@@ -0,0 +1,44 @@
+from models import JsonRPCResponse
+
+class Router:
+    def __init__(self,registry):
+        self.registry = registry
+
+    def route(self,request):
+        if request.method == "initialize":
+            return JsonRPCResponse(
+                id = request.id,
+                result = {
+                    "protocolVersion": "0.1",
+                    "serverInfo": {
+                        "name": "Scratch MCP",
+                        "version": "1.0"
+                    }
+
+                }
+            )
+        
+        elif request.method == "tools/list":
+            return JsonRPCResponse(
+                id=request.id,
+                result={
+                    "tools": self.registry.list_tools()
+                }
+            )
+        
+        elif request.method == "tools/call":
+            tool = self.registry.get_tool(request.params["name"])
+            output = tool.execute(request.params["arguments"])
+            return JsonRPCResponse(
+                id=request.id,
+                result=output
+            )
+        
+        else:
+            return JsonRPCResponse(
+                id=request.id,
+                error={
+                    "code": -32601,
+                    "message": "Method not found"
+                }
+            )
