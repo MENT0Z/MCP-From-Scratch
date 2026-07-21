770f514f02f34012d0ad72b55800f57fcd0e5b2c
Madan <mrmrupadhyau@gmail.com>
2026-07-21 11:16:58 +0545

    Implement Custom Calculator MCP server

---
+++ MCP USING SDK/servers/calculator.py
@@ -0,0 +1,22 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("Calculator Server")
+
+# here in the fast mcp whatever we wrote in the comment goes in the description field
+
+@mcp.tool()
+def add(a:int,b:int)->int:
+    """
+    tool that is used to add two numbers
+    """
+    return a+b
+
+@mcp.tool
+def multiply(a:int,b:int)->int:
+    """
+    tool that is used to multiply two numbers
+    """
+    return a*b
+
+if __name__ == "__main__":
+    mcp.run()
